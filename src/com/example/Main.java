98d02c60ceebfe843fdd566b52326a304d4fa569
Evan Coakley <evancoakley1@gmail.com>
2017-09-19 15:42:03 -0500

    complete

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,56 @@
 package com.example;
 
+import java.util.Scanner;
+
+import com.example.common.Month;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a month, typed in all Capital letters: ");
+
+        String userinput = scanner.nextLine();
+
+        List<String> monthList = new ArrayList<>();
+        monthList.add(userinput);
+
+        switch (userinput) {
+            case "JANUARY":
+                monthList.add(Month.JANUARY.getEnglishName());
+            case "FEBRUARY":
+                monthList.add(Month.FEBRUARY.getEnglishName());
+            case "MARCH":
+                monthList.add(Month.MARCH.getEnglishName());
+            case "APRIL":
+                monthList.add(Month.APRIL.getEnglishName());
+            case "MAY":
+                monthList.add(Month.MAY.getEnglishName());
+            case "JUNE":
+                monthList.add(Month.JUNE.getEnglishName());
+            case "JULY":
+                monthList.add(Month.JULY.getEnglishName());
+            case "AUGUST":
+                monthList.add(Month.AUGUST.getEnglishName());
+            case "SEPTEMBER":
+                monthList.add(Month.SEPTEMBER.getEnglishName());
+            case "OCTOBER":
+                monthList.add(Month.OCTOBER.getEnglishName());
+            case "NOVEMBER":
+                monthList.add(Month.NOVEMBER.getEnglishName());
+            case "DECEMBER":
+                monthList.add(Month.DECEMBER.getEnglishName());
+                break;
+            default:
+                System.out.println(userinput + " is not a month");
+        }
+        for (String monthName : monthList) {
+            System.out.println(monthName);
+        }
     }
+
+
 }
